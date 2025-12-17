88803a29dfab38ab41afc21099fda52f704f72f8
Steve Alex <salex@mac.com>
2025-12-17 14:50:47 -0600

    maybe fix lint errors

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -9,51 +9,51 @@ class AppsController < ApplicationController
 
   def places_sheet
     if params[:col].present?
-      render template: 'apps/places_sheet'
+      render template: "apps/places_sheet"
     else
-      render template: 'apps/places_sheet1'
+      render template: "apps/places_sheet1"
     end
   end
 
   def payouts
-    render template: 'apps/payouts/payouts'
+    render template: "apps/payouts/payouts"
   end
 
   def reset
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.replace('refresh', 
-          partial: 'apps/payouts/stats_refresh',
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("refresh", 
+          partial: "apps/payouts/stats_refresh",
           locals:{dues:params[:stats][:dues],
           meth:params[:stats][:game],
           doll:params[:stats][:doll],
           perc:params[:stats][:perc]})}
-      format.html { render :template => 'apps/payouts/payouts'}
+      format.html { render :template => "apps/payouts/payouts"}
     end
 
   end
 
   def about
-    render template: 'apps/payouts/about'
+    render template: "apps/payouts/about"
   end
   def deals
     render turbo_stream: turbo_stream.replace(
-      'content',
-      partial: 'apps/payouts/deals')
+      "content",
+      partial: "apps/payouts/deals")
   end
   def pga
     render turbo_stream: turbo_stream.replace(
-      'content',
-      partial: 'apps/payouts/pga')
+      "content",
+      partial: "apps/payouts/pga")
   end
   def rate
     render turbo_stream: turbo_stream.replace(
-      'content',
-      partial: 'apps/payouts/rate')
+      "content",
+      partial: "apps/payouts/rate")
   end
   def scoring
     render turbo_stream: turbo_stream.replace(
-      'content',
-      partial: 'apps/payouts/scoring')
+      "content",
+      partial: "apps/payouts/scoring")
   end
 
 
