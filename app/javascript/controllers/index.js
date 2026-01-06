63d78662b4be7e87ac27de4c3e735adfb05ae41c
Steve Alex <salex@mac.com>
2026-01-05 18:05:57 -0600

    going to try ssl
    moved json to hidden

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,6 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+import { Autocomplete } from 'stimulus-autocomplete'
+application.register('autocomplete', Autocomplete)
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,57 +10,51 @@
 
 # THIS IS AN INITIAL IMPORT OF THE LOCAL VERSION OF GOLFERS
 # PROBABLY CAN USED IT UPDATE NEW PLAYERS, GAMES AND ROUNDS
-json = File.read(Rails.root.join('db','golfers','set_golfers.json'))
-golfers = JSON.parse(json)
-puts golfers.size
-golfers.each{|i|
-  Golfer.create(i)
-}
+# json = File.read(Rails.root.join('db','golfers','set_golfers.json'))
+# golfers = JSON.parse(json)
+# puts golfers.size
+# golfers.each{|i|
+#   Golfer.create(i)
+# }
 
 
-json = File.read(Rails.root.join('db','golfers','set_stashes.json'))
-stashes = JSON.parse(json)
-puts stashes.size
-stashes.each{|i|
-  Stash.create(i)
-}
+# json = File.read(Rails.root.join('db','golfers','set_stashes.json'))
+# stashes = JSON.parse(json)
+# stashes.each{|i|
+#   Stash.create(i)
+# }
 
-json = File.read(Rails.root.join('db','golfers','set_groups.json'))
-groups = JSON.parse(json)
-puts groups.size
-groups.each{|i|
-  Group.create(i)
-}
+# json = File.read(Rails.root.join('db','golfers','set_groups.json'))
+# groups = JSON.parse(json)
+# groups.each{|i|
+#   Group.create(i)
+# }
 
-json = File.read(Rails.root.join('db','golfers','set_users.json'))
-users = JSON.parse(json)
-puts users.size
-users.each{|i|
-  User.create(i)
-}
+# json = File.read(Rails.root.join('db','golfers','set_users.json'))
+# users = JSON.parse(json)
+# users.each{|i|
+#   User.create(i)
+# }
 
-json = File.read(Rails.root.join('db','golfers','set_players.json'))
-players = JSON.parse(json)
-puts players.size
-players.each{|i|
-  Player.create(i)
-}
+# json = File.read(Rails.root.join('db','golfers','set_players.json'))
+# players = JSON.parse(json)
+# players.each{|i|
+#   Player.create(i)
+# }
 
-json = File.read(Rails.root.join('db','golfers','set_games.json'))
-games = JSON.parse(json)
-puts games.size
-games.each{|i|
-  Game.create(i)
-}
+# json = File.read(Rails.root.join('db','golfers','set_games.json'))
+# games = JSON.parse(json)
+# games.each{|i|
+#   Game.create(i)
+# }
 
-json = File.read(Rails.root.join('db','golfers','set_rounds.json'))
-rounds = JSON.parse(json)
-puts rounds.size
-rounds.each{|i|
-  Round.create(i)
-}
+# json = File.read(Rails.root.join('db','golfers','set_rounds.json'))
+# rounds = JSON.parse(json)
+# rounds.each{|i|
+#   Round.create(i)
+# }
 
-# get_golfers
+# # get_golfers
 # get_stashes
 # get_groups
 # get_users
